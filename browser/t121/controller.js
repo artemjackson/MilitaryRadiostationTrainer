101761be0e4ff44112cbe61439c175901bcdfd82
Artem Jackson <artemjackson95@gmail.com>
2015-10-18 20:19:07 +0300

    Added configuration for knob-controls

--- browser/t121/controller.js
+++ browser/t121/controller.js
@@ -1,4 +1,23 @@
 "use strict";
 
+import config from "knobs.config.json";
+
 export default class T121Controller {
+    constructor() {
+        this.frictionErrorPositive = {
+            config: config.frictionErrorPositive
+        };
+
+        this.latitude = {
+            config: config.latitude
+        };
+
+        this.frictionErrorNegative = {
+            config: config.frictionErrorNegative
+        };
+
+        this.balance = {
+            config: config.balance
+        };
+    }
 }
