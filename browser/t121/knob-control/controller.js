5255fce552958b599232125a1324d30b9ba73154
Artem Jackson <artemjackson95@gmail.com>
2015-10-18 20:16:27 +0300

    Fixed knob-control representation
    Deleted separated derictives for each knob-control
    Created common configurable knob-control directive

---
+++ browser/t121/knob-control/controller.js
@@ -0,0 +1,50 @@
+"use strict";
+
+export default class KnobControlCtrl {
+    constructor() {
+        this.marks = this.config.marks;
+        this.rotationSpeed = this.config.rotationSpeed;
+
+        this.minValue = this.marks[0].value;
+        this.maxValue = this.marks[this.marks.length - 1].value;
+
+        this.value = this.minValue;
+    }
+
+    get value() {
+        return this._value;
+    }
+
+    set value(value) {
+        this._value = value;
+
+        value = Number(value);
+
+        if (this.marks) {
+            for (let i = 1; i < this.marks.length; ++i) {
+                const leftMark = this.marks[i - 1];
+                const rightMark = this.marks[i];
+
+                if (value >= leftMark.value && value <= rightMark.value) {
+                    const angleRange = rightMark.angle - leftMark.angle;
+                    const valueRange = rightMark.value - leftMark.value;
+                    const angleOnValue = angleRange / valueRange;
+
+                    this.currentAngle = leftMark.angle + (value - leftMark.value) * angleOnValue;
+                }
+            }
+        }
+    }
+
+    increase() {
+        if (this.value < this.maxValue) {
+            this.value++;
+        }
+    }
+
+    decrease() {
+        if (this.value > this.minValue) {
+            this.value--;
+        }
+    }
+}
--- browser/t121/knob-control/index.js
+++ browser/t121/knob-control/index.js
@@ -1,18 +1,19 @@
 "use strict";
 
 import angular from "angular";
-import BalanceKnobControlCtrl from "./controller";
+import KnobControlCtrl from "./controller";
 
-export default angular.module("t121.knob-controls.balance", [])
-    .directive("t121BalanceKnobControl", () => {
+export default angular.module("t121.knob-control", [])
+    .directive("t121KnobControl", () => {
         return {
             bindToController: true,
-            controller: BalanceKnobControlCtrl,
+            controller: KnobControlCtrl,
             controllerAs: 'ctrl',
             replace: true,
             restrict: "A",
             scope: {
-                currentValue: "="
+                config: "=",
+                value: "="
             },
             template: require("./template.html")
         }
